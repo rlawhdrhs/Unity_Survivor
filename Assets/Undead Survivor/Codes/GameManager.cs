fb1b7a05ce519350bdf25ba50096593d819488d9
rlawhdrhs <kimjg000105@naver.com>
2025-01-02 04:00:38 +0900

    1

---
+++ Assets/Undead Survivor/Codes/GameManager.cs
@@ -0,0 +1,113 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    [Header("# Game Control")]
+    public bool isLive;
+    public float gameTime;
+    public float maxGameTime = 2 * 10f;
+    [Header("# Player Info")]
+    public float health;
+    public float maxhealth = 100;
+    public int level;
+    public int kill;
+    public int exp;
+    public int[] nextExp = { 10, 30, 60, 100, 150, 210, 280, 360, 450, 600 };
+    [Header("# Game Object")]
+    public PoolManager pool;
+    public Player player;
+    public Result uiResult;
+    public GameObject enemyCleaner;
+    public Weapon weapon1, weapon2;
+    public float damP = 0;
+
+    void Awake()
+    {
+        instance = this;
+        Stop();
+    }
+    public void GameStart()
+    {
+        health = maxhealth;
+        isLive = true;
+        Resume();
+    }
+    public void Gameover()
+    {
+        StartCoroutine(GameOverRoutine());
+    }
+
+    IEnumerator GameOverRoutine()
+    {
+        isLive = false;
+        yield return new WaitForSeconds(0.5f);
+        uiResult.gameObject.SetActive(true);
+        uiResult.Lose();
+        Stop();
+    }
+    public void GameVictory()
+    {
+        StartCoroutine(GameVictoryRoutine());
+    }
+
+    IEnumerator GameVictoryRoutine()
+    {
+        isLive = false;
+        enemyCleaner.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+        uiResult.gameObject.SetActive(true);
+        uiResult.Win();
+        Stop();
+    }
+    public void GameRetry()
+    {
+        SceneManager.LoadScene(0);
+        Resume();
+    }
+    void Update()
+    {
+        if (!isLive)
+            return;
+
+
+        gameTime += Time.deltaTime;
+
+        if (gameTime > maxGameTime)
+        {
+            gameTime = maxGameTime;
+            GameVictory();
+        }
+    }
+
+    public void GetExp()
+    {
+        if (!isLive)
+            return;
+
+        exp++;
+        if(exp == nextExp[level])
+        {
+            level++;
+            exp = 0;
+            damP++;
+            weapon1.LevelUp(weapon1.damage + damP, 1);
+            weapon2.LevelUp(weapon2.damage + damP, 1);
+        }
+    }
+
+    public void Stop()
+    {
+        isLive = false;
+        Time.timeScale = 0;
+    }
+
+    public void Resume()
+    {
+        isLive = true;
+        Time.timeScale = 1;
+    }
+}
---
+++ Assets/Undead Survivor/Codes/Player.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Vector2 inputVec;
+    public float speed;
+    public Scanner scanner;
+    // Start is called before the first frame update
+
+    Rigidbody2D rigid;
+    SpriteRenderer spriter;
+    Animator anim;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+        scanner = GetComponent<Scanner>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputVec.x = Input.GetAxisRaw("Horizontal");
+        inputVec.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime;
+        rigid.MovePosition(rigid.position + nextVec);
+    }
+
+    private void LateUpdate()
+    {
+        anim.SetFloat("Speed", inputVec.magnitude);
+        if(inputVec.x != 0) {
+            spriter.flipX = inputVec.x < 0 ? true : false;
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (!GameManager.instance.isLive)
+            return;
+
+        GameManager.instance.health -= Time.deltaTime * 10;
+
+        if (GameManager.instance.health < 0)
+        {
+            for (int index = 2; index < transform.childCount; index++)
+            {
+                transform.GetChild(index).gameObject.SetActive(false);
+            }
+
+            anim.SetTrigger("Dead");
+            GameManager.instance.Gameover();
+        }
+    }
+}
